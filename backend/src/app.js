298ff367608fd95ebad006e17de6a9064b69c6c4
6631501147 <6631501147@lamduan.mfu.ac.th>
2026-09-25 15:01:02 +0700

    fix: use regex catch-all for Express 5 path-to-regexp compatibility

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ app.use('/api/transactions', transactionRoutes);
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 // Catch-all route for Vue Router history mode
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
